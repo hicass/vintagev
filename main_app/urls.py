4364260ed4ce646c778382ede85d943ff559377f
Ongcangco <POngcangco57@gmail.com>
2023-06-06 20:31:51 -0400

    Add url

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
     path('clothes/', views.clothes_index, name='index'),
+    path('clothes/<int:pk>/update/', views.ClothingUpdate.as_view(), name='clothes_update'),
+    path('clothes/<int:pk>/delete/', views.ClothingDelete.as_view(), name='clothes_delete'),
+    path('clothes/bag', views.bag, name='bag'),
+    path('clothes/likes', views.likes, name='likes'),
 ]

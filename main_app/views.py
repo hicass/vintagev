07620c794a1a4438f0a8916c4a8b207495c0decf
hicass <cassidyjwalters@gmail.com>
2023-06-07 16:29:12 -0700

    Add functionality for category nav links thats filter by each category.

--- main_app/views.py
+++ main_app/views.py
@@ -35,7 +35,7 @@ class ClothesCreate(LoginRequiredMixin, CreateView):
   fields = ['clothing_name', 'brand', 'category', 'size', 'condition', 'material', 'color', 'description', 'price']
   def form_valid(self, form):
     form.instance.user = self.request.user
-    return redirect(request, '/')
+    return super().form_valid(form)
 
 
 class ClothesUpdate(LoginRequiredMixin, UpdateView):
@@ -80,7 +80,7 @@ def add_photo(request, clothes_id):
             bucket = os.environ['S3_BUCKET']
             s3.upload_fileobj(photo_file, bucket, key)
             url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
-            Photo.objects.create(url=url, clothes_id=clothes_id)
+            Photo.objects.create(url=url, clothing_id=clothes_id)
         except Exception as e:
             print('An error occurred uploading file to S3')
             print(e)
@@ -89,10 +89,58 @@ def add_photo(request, clothes_id):
 
 
 def clothes_detail(request, clothes_id):
-    clothing = Clothes.objects.get(id=clothes_id)
+    clothes = Clothes.objects.get(id=clothes_id)
 
     return render(request, 'clothes/detail.html', {
-        'clothing': clothing
+        'clothes': clothes
     })
 
 
+
+def tops_index(request):
+    clothing = Clothes.objects.filter(category='T')
+    return render(request, 'clothes/index.html', {
+        'clothing': clothing
+    })
+
+def bottoms_index(request):
+    clothing = Clothes.objects.filter(category='Btm')
+    return render(request, 'clothes/index.html', {
+        'clothing': clothing
+    })
+
+def jackets_index(request):
+    clothing = Clothes.objects.filter(category='Jckt')
+    return render(request, 'clothes/index.html', {
+        'clothing': clothing
+    })
+
+def dresses_index(request):
+    clothing = Clothes.objects.filter(category='Drs')
+    return render(request, 'clothes/index.html', {
+        'clothing': clothing
+    })
+
+def jumpsuits_index(request):
+    clothing = Clothes.objects.filter(category='Jmp')
+    return render(request, 'clothes/index.html', {
+        'clothing': clothing
+    })
+
+def shoes_index(request):
+    clothing = Clothes.objects.filter(category='Sh')
+    return render(request, 'clothes/index.html', {
+        'clothing': clothing
+    })
+
+def shoes_index(request):
+    clothing = Clothes.objects.filter(category='Sh')
+    return render(request, 'clothes/index.html', {
+        'clothing': clothing
+    })
+
+def accessories_index(request):
+    clothing = Clothes.objects.filter(category='Acc')
+    return render(request, 'clothes/index.html', {
+        'clothing': clothing
+    })

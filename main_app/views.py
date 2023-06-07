b946bfacfd64e0efad3d49a0e92c746d50e9cb9c
mr-duk25 <samuelrobles@Samuels-MacBook-Pro.local>
2023-06-07 10:28:38 -0700

    Added add-photo function

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from .models import Clothes
+from .models import Clothes, Photo
+import uuid
+import boto3
+import os
+
 
 
 def home(request):
@@ -57,3 +61,23 @@ def signup(request):
     context = {'form': form, 'error_message': error_message}
 
     return render(request, 'registration/signup.html', context)
+
+
+
+
+
+
+def add_photo(request, clothing_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            bucket = os.environ['S3_BUCKET']
+            s3.upload_fileobj(photo_file, bucket, key)
+            url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+            Photo.objects.create(url=url, clothing_id=clothing_id)
+        except Exception as e:
+            print('An error occurred uploading file to S3')
+            print(e)
+    return redirect('detail', clothing_id=clothing_id)

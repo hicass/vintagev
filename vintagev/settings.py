ae83d46a85e36d275a1ebef3d8e52539bb63a617
hicass <cassidyjwalters@gmail.com>
2023-06-08 13:22:36 -0700

    Deployment

--- vintagev/settings.py
+++ vintagev/settings.py
@@ -135,4 +135,4 @@ LOGOUT_REDIRECT_URL = '/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import django_on_heroku
-django_on_herok.settings(locals())
+django_on_heroku.settings(locals())

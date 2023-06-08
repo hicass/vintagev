4890cef239221bf01fb4b175543bf130348e31cf
hicass <cassidyjwalters@gmail.com>
2023-06-08 14:33:50 -0700

    Deploy

--- vintagev/settings.py
+++ vintagev/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'NAME': 'vintagev',
     'USER': os.getenv('DB_USER'),
     'PASSWORD': os.getenv('DB_PW'),
-    'HOST': os.getenv('DB_HOST'),
+    'HOST': os.getenv('DATABASE_URL'),
     'PORT': '5432',
   }
 }
